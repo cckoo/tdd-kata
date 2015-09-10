7acb052d1da3977252305651a274a6d3c8eb276a
cckoo <595743688@qq.com>
2015-09-10 11:14:57 +0800

    完成业务代码

--- Tennis/KataTennis.php
+++ Tennis/KataTennis.php
@@ -15,20 +15,77 @@ class KataTennis {
     }
 
     private function start() {
-        $this->score = 'love all';
+        $this->score = array(0,0);
+    }
+
+    private function translateScoreToString($score) {
+        switch ($score) {
+            case 0:
+                $res = "love ";
+                break;
+            case 1:
+                $res = "fifteen ";
+                break;
+            case 2:
+                $res = "thirty ";
+                break;
+            case 3:
+                $res = "forty ";
+                break;
+            default:
+                $res = $score;
+        }
+
+        return $res;
     }
 
     public function getScore() {
-        return $this->score;
+        $player1Score = $this->translateScoreToString($this->score[0]);
+        $player2Score = $this->translateScoreToString($this->score[1]);
+        if (is_numeric($player1Score) || is_numeric($player2Score)) {
+            if ($player1Score === $player2Score)
+                return 'deuce';
+
+            if ($player1Score !== 'forty ' && !is_numeric($player1Score))
+                return $this->player2 . " win";
+
+            if ($player2Score !== 'forty ' && !is_numeric($player2Score))
+                return $this->player1 . " win";
+
+            if ($player1Score === 'forty ' && is_numeric($player2Score) && $player2Score >= 5)
+                return $this->player2 . " win";
+
+            if ($player2Score === 'forty ' && is_numeric($player1Score) && $player1Score >= 5)
+                return $this->player1 . " win";
+
+            if (is_numeric($player1Score) && is_numeric($player2Score)){
+                if ($player1Score - $player2Score >= 2)
+                    return $this->player1 . " win";
+                if ($player2Score - $player1Score >= 2)
+                    return $this->player2 . " win";
+            }
+
+            return $player1Score > $player2Score ? $this->player1 . " advantage" : $this->player2 . " advantage";
+        }
+        if ($player1Score === $player2Score) {
+
+            if ($player1Score === 'forty ') {
+                return 'deuce';
+            }
+
+            return $player1Score . "all";
+        }
+
+        return  trim($player1Score . $player2Score);
     }
 
     public function whoGetThisScore($player) {
         switch ($player) {
             case $this->player1:
-                $this->score = 'fifteen love';
+                $this->score[0] += 1;
                 break;
             case $this->player2:
-                $this->score = 'love fifteen';
+                $this->score[1] += 1;
                 break;
         }
     }
--- Tennis/KataTennisTest.php
+++ Tennis/KataTennisTest.php
@@ -18,4 +18,133 @@ class KataTennisTest extends PHPUnit_Framework_TestCase {
         $tennis->whoGetThisScore('cckoo');
         $this->assertEquals("love fifteen", $tennis->getScore());
     }
+
+    public function test_player_all_win_score_should_be_fifteen_all() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("fifteen all", $tennis->getScore());
+    }
+
+    public function test_player1_win_twice_score_should_be_thirty_love() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("thirty love", $tennis->getScore());
+    }
+
+    public function test_player2_win_twice_score_should_be_love_thirty() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("love thirty", $tennis->getScore());
+    }
+
+    public function test_player_all_win_twice_score_should_be_thirty_all() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("thirty all", $tennis->getScore());
+    }
+
+    public function test_player1_win_three_times_score_should_be_forty_love() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("forty love", $tennis->getScore());
+    }
+
+    public function test_player1_win_three_times_score_should_be_love_forty() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("love forty", $tennis->getScore());
+    }
+
+    public function test_player_all_win_three_times_score_should_be_forty_all() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("deuce", $tennis->getScore());
+    }
+
+    public function test_when_deuce_player1_win_score_should_be_player1name_advantage() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("cwcwk advantage", $tennis->getScore());
+    }
+
+    public function test_when_deuce_player2_win_score_should_be_player2name_advantage() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $this->assertEquals("cckoo advantage", $tennis->getScore());
+    }
+
+    public function test_when_deuce_player1_win_and_player2_win_score_should_be_deuce() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("deuce", $tennis->getScore());
+    }
+
+    public function test_when_player1_win_four_times_and_player2_didnt_win_more_than_three_times_should_be_player1_win() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("cwcwk win", $tennis->getScore());
+    }
+
+    public function test_when_player2_win_four_times_and_player1_didnt_win_more_than_three_times_should_be_player2_win() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("cckoo win", $tennis->getScore());
+    }
+
+    public function test_when_deuce_player1_win_twice_score_should_be_player1_win() {
+        $tennis = new KataTennis("cwcwk", "cckoo");
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cckoo');
+        $tennis->whoGetThisScore('cwcwk');
+        $tennis->whoGetThisScore('cwcwk');
+        $this->assertEquals("cwcwk win", $tennis->getScore());
+    }
 }
